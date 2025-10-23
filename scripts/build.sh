464007e64f87ead9d5b21a514fe4c519500f6038
root <root@nas-server.localdomain>
2025-10-23 18:31:30 +0800

    优化代码

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 dirname=$(dirname $(dirname $(readlink -f $0)))
+project_name=$(basename $dirname)
+
 tree $dirname -I dist -I __pycache__ -I dist -I build -I *.egg-info -I config -I data -I *.log > $dirname/package_structure.txt
-sed -i "s#$dirname#ftp_transfer#g" $dirname/package_structure.txt > $dirname/ftp_transfer_files.txt
+sed -i "s#$dirname#$project_name#g" $dirname/package_structure.txt > $dirname/$project_name.txt
+rm $dirname/$project_name.txt
 python $dirname/setup.py bdist_wheel --dist-dir $dirname/dist
